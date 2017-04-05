7fc3eb7935910352b8d718969f284ef16b3afa21
Honza <jan.smid@sky.uk>
2017-04-05 17:07:58 +0200

    cleared

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <form>
+          <button type="submit">Submit</button>
+        </form>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Form from './Form';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Form />,
   document.getElementById('root')
 );
