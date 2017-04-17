8f2ebaa6f594f79813cda5f7f29c539b7c4abefc
Honza <jan.smid@sky.uk>
2017-04-17 22:32:55 +0200

    added validation rules

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,24 +1,85 @@
 const isEmpty = value => value === null || value === undefined || value === '';
 
-export default (fieldValue = '', validation = [], formData) => {
-  const errors = [];
+const getErrorIfDoesNotMatchRegEx = (fieldValue, patterns) => {
+  if (fieldValue.length === 0) {
+    return null;
+  }
 
-  validation.forEach(({ type, rules }) => {
-    let hasError = false;
-    let validationErrorText = rules[0].message;
+  const errors = patterns
+    .filter(pattern => fieldValue.match(pattern.value) === null)
+    .map(field => field.message);
 
-    switch (type) {
-      case 'required':
-        hasError = isEmpty(fieldValue);
-        break;
-      default:
-        hasError = false;
-    }
+  return (errors.length > 0) ? errors[0] : null;
+};
 
-    if (hasError) {
-      errors.push(validationErrorText);
-    }
-  });
+const getErrorIfMatchesRegEx = (fieldValue, patterns) => {
+  if (fieldValue.length === 0) {
+    return null;
+  }
 
-  return errors.length > 0 ? errors[0] : '';
+  const errors = patterns
+    .filter(pattern => fieldValue.match(pattern.value) !== null)
+    .map(field => field.message);
+
+  return (errors.length > 0) ? errors[0] : null;
+};
+
+const isNotEqualToExpectedValue = (fieldValue, expectedValue) => fieldValue !== expectedValue;
+
+const isInList = (fieldValue, allowedValues) => allowedValues.indexOf(fieldValue) > -1;
+
+const isGreaterThanMaxLength = (value, maxLength) => value.length > maxLength;
+
+const isLessThanMinLength = (value, minLength) => value.length < minLength;
+
+const doesNotMatch = (fieldValue, fieldName, formData) => formData[fieldName] !== fieldValue;
+
+const isNotEqualCaseInsensitive = (fieldValue, fieldName, formData) => formData[fieldName].toLowerCase() !== fieldValue.toLowerCase();
+
+export default (fieldValue = '', validation = [], formData = {}) => {
+  const rule = validation
+    .map(({ type, rules }) => {
+      const ruleValue = rules[0].value;
+      const errorMessage = rules[0].message;
+
+      switch (type) {
+        case 'required':
+          return isEmpty(fieldValue) ? errorMessage : null;
+          break;
+        case 'minlength':
+          return isLessThanMinLength(fieldValue, ruleValue) ? errorMessage : null;
+          break;
+
+        case 'maxlength':
+          return isGreaterThanMaxLength(fieldValue, ruleValue) ? errorMessage : null;
+          break;
+
+        case 'mustbeequal':
+          return isNotEqualToExpectedValue(fieldValue, ruleValue) ? errorMessage : null;
+          break;
+
+        case 'inlist':
+          return !isInList(fieldValue, ruleValue) ? errorMessage : null;
+          break;
+
+        case 'pattern':
+            return getErrorIfDoesNotMatchRegEx(fieldValue, rules);
+          break;
+
+        case 'notpattern':
+            return getErrorIfMatchesRegEx(fieldValue, rules);
+          break;
+
+        case 'mustmatch':
+          return doesNotMatch(fieldValue, ruleValue, formData) ? errorMessage : null;
+          break;
+
+        case 'mustmatchcaseinsensitive':
+          return isNotEqualCaseInsensitive(fieldValue, ruleValue, formData) ? errorMessage : null;
+          break;
+      }
+    })
+    .filter(error => error !== null);
+
+  return rule.length > 0 ? rule[0] : '';
 }
