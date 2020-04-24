018f07f624bef51655a1ec791bf2a2b3d7b14f50
smidhonza <jansmidhonza@gmail.com>
2020-04-24 10:57:14 +0000

    Updates

---
+++ precache-manifest.d443ff92fdd79c52bd7a324112afd8f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee8ea359bcabbc8f51000717f0e1ae3",
+    "url": "/metacharts/index.html"
+  },
+  {
+    "revision": "0751e178525d96c6faa4",
+    "url": "/metacharts/static/css/main.a03fec9e.chunk.css"
+  },
+  {
+    "revision": "8ddebedccb1557e0b036",
+    "url": "/metacharts/static/js/2.50f80c5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/metacharts/static/js/2.50f80c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0751e178525d96c6faa4",
+    "url": "/metacharts/static/js/main.ecd9650d.chunk.js"
+  },
+  {
+    "revision": "25d03e8b44637e40a32d",
+    "url": "/metacharts/static/js/runtime-main.7b7eeb46.js"
+  }
+]);
