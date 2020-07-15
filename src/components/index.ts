c41da908ab77e5d6a162dc9f930d4797e5791baf
Honza Smid <jansmidhonza@gmail.com>
2020-07-15 11:19:34 +0200

    latest metaforms, NumberInput component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Form } from './Form';
 
 export { default as Input } from './fields/Input';
+export { default as NumberInput } from './fields/NumberInput';
 export { default as Textarea } from './fields/Textarea';
 export { default as Checkbox } from './fields/Checkbox';
 export { default as Select } from './fields/Select';
